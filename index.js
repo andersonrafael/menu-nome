3ab5351209db4c919b68cd860397ae0e7df7bcd5
francisco anderson <franciscoandersonrafael@gmail.com>
2025-11-14 23:21:31 -0300

    alteracao de layout

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 
-const btnMobile = document.getElementById("btn-mobile");
+/*const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu() {
-    const nav = document.getElementById("nav");
+    const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
-btnMobile.addEventListener("click", function (toggleMenu));
+btnMobile.addEventListener('click', (toggleMenu));
+*/
+const btnMobile = document.getElementById('btn-mobile');
+const menu = document.getElementById('menu');
 
+// Adiciona um evento de clique ao botão "Menu"
+btnMobile.addEventListener('click', function () {
+    // Alterna a classe "active" no menu
+    menu.classList.toggle('active');
+});
